118c4ef30334a60cceb6d053e5d4d67fa0aa9b6f
Tyuiu.DolgushinVA@outlook.com <vadimdolgushin9@gmail.com>
2023-10-24 19:18:55 +0500

    Task5

---
+++ Tyuiu.DolgushinVA.Sprint3.Task5.V27.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DolgushinVA.Sprint3.Task5.V27.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DolgushinVA.Sprint3.Task5.V27.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9bdf6a1c-691b-483b-8f4f-072267610006")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
