0a530d2ee86abcf419de34c84b253841e75b92bd
Tyuiu.DolgushinVA@outlook.com <vadimdolgushin9@gmail.com>
2023-11-16 00:39:35 +0500

    Task 7 Fix

--- Tyuiu.DolgushinVA.Sprint3.Task7.V14.Lib/DataService.cs
+++ Tyuiu.DolgushinVA.Sprint3.Task7.V14.Lib/DataService.cs
@@ -18,6 +18,10 @@ namespace Tyuiu.DolgushinVA.Sprint3.Task7.V14.Lib
             for (int x = startValue; x <= stopValue; x++)
             {
                 double y = Math.Round(2 * x - 4 + ((2 * x - 1) / (Math.Sin(x) + 1)), 2);
+                if ((Math.Sin(x) + 1) == 0)
+                {
+                    y = 0;
+                }
                 valueArray[count] = y;
                 count++;
             }
